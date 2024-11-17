95b15c2960e3cfe199e432313ae0db441616f940
josedomingos919 <josedomingos919@gmail.com>
2024-11-17 17:50:28 +0100

    feat: finished header session

---
+++ main.js
@@ -0,0 +1,27 @@
+// Toggle Button
+const navMenu   = document.getElementById("nav-menu");
+const navLink   = document.querySelectorAll(".nav-link");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener('click', ()=>{
+    navMenu.classList.toggle('left-[0]');
+    hamburger.classList.toggle('ri-close-large-line');
+});
+
+navLink.forEach((link)=>{
+    link.addEventListener("click", ()=>{
+        navMenu.classList.toggle('left-[0]');
+        hamburger.classList.toggle('ri-close-large-line');
+    });
+});
+
+
+// Show scroll up
+
+// Change Background Header
+
+// Swiper
+
+// Scroll sections active link
+
+// Scroll reveal animation
